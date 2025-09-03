2e5377ba70b1394b21742ce8aa4225d26451e93b
saken555 <exprechi.p@gmail.com>
2025-09-03 18:34:51 +0500

    Update db.php

--- api/conf/db.php
+++ api/conf/db.php
@@ -1,1 +1,27 @@
+<?php
+class Database {
+    // Параметры подключения к БД
+    private $host = 'localhost';
+    private $db_name = '';
+    private $username = '';
+    private $password = '';
+    private $conn;
 
+    
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
